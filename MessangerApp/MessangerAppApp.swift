f2a4a27da29f8c1a7ce208391cc4f147be9b27f0
Ivan Durmashev <ivandurmashev@gmail.com>
2022-12-03 14:26:01 +0300

    Initial Commit

---
+++ MessangerApp/MessangerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerAppApp.swift
+//  MessangerApp
+//
+//  Created by Ivan Durmashev on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MessangerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
